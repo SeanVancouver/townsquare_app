310aa81c723c35846760b386db48374b94ed13a1
SeanVancouver <luoxiao1106@gmail.com>
2024-10-13 16:15:40 -0700

    finished elites

--- app/why/elites/page.js
+++ app/why/elites/page.js
@@ -1,11 +1,44 @@
-import WhyHeader from '@/components/why/WhyHeader'
+import Link from 'next/link';
+import AudioPlayer from '@/components/AudioPlayer';
 
 const Index = () => {
   return (
     <>
-    <WhyHeader />
-    <div>Elites</div>
-  </>
+      <AudioPlayer />
+      <div>
+        <p>In 1958, the Chinese Communist Party launched the Great Leap Forward, where private farms where replaced by communal ones with harsh production quotas. The peasants were told that the quota were set according to the production level of other villages, and often have their crops forcefully confisticated to meet it.</p>
+        <p><br /></p>
+        <p>Everyone thought their village is the only one underperforming, so any resistance is futile. But in reality, almost all villages across China were suffering the exact same fate. As result, up to 55 millions starved in the span of three years.</p>
+        <p><br /></p>
+        <p>Had the villagers being able to communicate with each other, they would have realized the extend of the problem, and perhaps start a widespread revolt. But they couldn&apos;t, because any dictatorship worth their salt knows their hold onto power lies in the absolute control of information.</p>
+        <p><br /></p>
+        <p>So how do the dicators of the East compare with the elites of the West? Having lived in both East and West, I can say that the Western elites operates at a higher level with more succint and skills.</p>
+        <p><br /></p>
+        <p>They are very well aware of the discontent of the populace, but instead of suppressing it and risk exposing themselves, they prefer to own and commandeer it, like a controlled opposition.</p>
+        <p><br /></p>
+        <p>Just think about how many Youtube videos are popping up nowadays complaining about the wealth disparity and the impending collapse. They all share the same patterns:</p>
+        <p><br /></p>
+        <p>&nbsp; &nbsp; 1. Describe the problem beautifully, but propose no real solution other than a dire warning.</p>
+        <p><br /></p>
+        <p>&nbsp; &nbsp; 2. And when they do propose a solution, it is always one of these two types:</p>
+        <p><br /></p>
+        <p>&nbsp; &nbsp; &nbsp; &nbsp; - Some kind of systematic solution that is too much to implement for its supposed benefit.&nbsp;</p>
+        <p><br /></p>
+        <p>&nbsp; &nbsp; &nbsp; &nbsp; - The &quot;How to save yourself and your loved ones&quot; kind.</p>
+        <p>&nbsp; &nbsp;&nbsp;</p>
+        <p>The purpose is to condition us to accept the wealth inequality and the impending collapse. It further divides us by rewiring our brain so that even when we try to find a solution, we think self-preservation and rat race.</p>
+        <p><br /></p>
+        <p>TownSquare is exactly designed to break the spell by doing the opposite, by empowering and uniting the people. There is no gatekeeping to the number of people one can reach. At a click of a button one can broadcast to the entire city to find and organize with like minded people.</p>
+        <p><br /></p>
+        <p>You see, the question of wealth inequality is too broad for the society to achieve a consensus. College student, parents, immigrant and everyone else experience it differently. What TownSquare does, is to lower the threshold required for taking actions. So instead of arguing about the &quot;right&quot; solution into oblivion, everyone can find their tribe and implement their own best solution.</p>
+        <p><br /></p>
+        <p>And this is how we can take down the elites, brick by brick. Every little empowerment for the population chips away their control, and it all starts with TownSquare.</p>
+      </div>
+
+      <div>
+        <p>Next reason: <Link href="/why/peole">People</Link></p>
+      </div>
+    </>
   )
 }
 
--- app/why/empowerment/page.js
+++ app/why/empowerment/page.js
@@ -6,24 +6,33 @@ const Index = () => {
     <>
       <AudioPlayer />
       <div>
-        <p>Would you like to have superpower? Not like flying or anything, but a power that, when yield properly, can bring you more of whatever you wanted, whether money, fame, friends, or else.</p>
+        <p>Would you like to have superpower? Not like flying or anything, but a power that, when yield properly, can make your whatever dream of yours come true.</p>
+        <p><br /></p>
         <p>What TownSquare can offer you, is the ability to instantly reach the entire population of your city. Instead of interacting on the usual person to person basis, you can now broadcast to the entire city, and hear the city echo back to you.</p>
-        <p>Just imagine the leverage this gives you. This is having an equivalent reach of a top tier influencer or Youtuber who have millions of followers/subscribers, except that you never have to work for it.</p>
-        <p>Here are three scenarios on how you may use your newfound power.</p>
+        <p><br /></p>
+        <p>Just imagine the leverage this gives you. This is having an equivalent reach of a top tier influencer or Youtuber who have millions of followers/subscribers, except that you never have to work for it. Whether you have an idea to spread or a project to create, you can now find like minded people city wide at the click of a button. Here are just three examples of usageL</p>
+        <p><br /></p>
         <p>1. Seeking</p>
         <p>&nbsp; &nbsp; Say that you are a tough looking bearded biker, but Hello Kitty is your secret guilty pleasure. You can&apos;t join any hobby groups because your appearance scares the participants who are mostly little girls.</p>
-        <p>&nbsp; &nbsp; But with TownSquare, you can now filter the entire city&apos;s population, for other Hello Kitty loving bikers. There may not be many, but at least now you can find your niche tribe.</p>
+        <p><br /></p>
+        <p>&nbsp; &nbsp; But with TownSquare, you can now filter the entire city&apos;s population, for other Hello Kitty loving bikers. Then you get a map of people matching the criteria. There may not be many, but at least now you can find your niche tribe.</p>
+        <p><br /></p>
         <p><br /></p>
         <p>2. Browsing</p>
         <p>&nbsp; &nbsp; Sometimes we don&apos;t know what exactly we are looking for, and just want to browse to see what is available. No problem. At TownSquare, you can scroll to see posts just like with Twitter or Instagram, with the exception that all posts are made by people from your city.</p>
+        <p><br /></p>
         <p>&nbsp; &nbsp; Of course you can also customize or prioritize what you see. For example, you can filter for only posts about a certain topics, or from authors matching a certain criteria, like Hello Kitty loving bikers.</p>
+        <p><br /></p>
         <p><br /></p>
         <p>3. Partaking</p>
         <p>&nbsp; &nbsp; And if you want to hang out with people live, TownSquare offers virtual townhalls that caters to different groups.</p>
+        <p><br /></p>
         <p>&nbsp; &nbsp; We may for example, have a fun town hall for live football matches, or a support town hall for first time moms. Whoever you are and whatever your interests are, it will feel like you can step into your kind of party whenever you want.</p>
         <p><br /></p>
-        <p>These are just three examples to illustrate, the reality is that the possibilities are endless. And it is all possible because TownSquare limits the service area to that of a city. Because we don&apos;t have to think about scaling geographically, we can implement as much features as we desire, thus empowering everyone and bringing back fun and community.</p>
-        <p>This ability to see and reach everyone was previously only held by the big data companies, but now it is in your hand, and you can see your city as the true lively beast it is.</p>
+        <p><br /></p>
+        <p>These are just three examples to illustrate, the reality is that the possibilities are endless. And it is all possible because TownSquare limits the service area to that of a city. Because we don&apos;t have to think about scaling geographically, we can implement as much features as we desire, whether it is allowing money transfer between users, or API integration with other platforms. Sky is the limit.</p>
+        <p><br /></p>
+        <p>This unprecedented power was previously only held by the big data companies, but now it is in the palm of your hand. Of course everyone else will share this power, but this is what brings back the community and make our cities alive again.</p>
       </div>
 
       <div>
