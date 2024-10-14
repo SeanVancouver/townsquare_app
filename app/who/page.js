0d27da815364d3856d08ebb9613d456fb5d650f5
SeanVancouver <luoxiao1106@gmail.com>
2024-10-14 13:49:31 -0700

    done about me

--- app/who/page.js
+++ app/who/page.js
@@ -4,9 +4,31 @@ const Index = () => {
   return (
     <>
       <div>
-        <h2>You:</h2>
-        <p>[want you poster]</p>
-        <p>I cannot accomplish TownSquare alone. If this is something that interests you, please consider joining the movement here.</p>
+        <p>I am a queer immigrant who mostly grew up in Canada. My university years were one of great discoveries about myself and others.&nbsp;</p>
+        <p><br /></p>
+        <p>One of the discovery I made, is that many people with whom I share a lot in common with can be homophobic, and I had to turn away from many friendships because of this.</p>
+        <p><br /></p>
+        <p>This prompted me to wonder, if I had been straight, would I have been homophobic too? And more importantly, is it possible that I too am being behaving unjustly towards any group of people?&nbsp;</p>
+        <p><br /></p>
+        <p>This made me more empathetic and I started paying attention to my actions and the suffering of others.</p>
+        <p><br /></p>
+        <p>After graduation I worked in finance, and that&apos;s when I saw the one thing that causes the most harm to our society, but deemed acceptable because everyone does it.&nbsp;</p>
+        <p><br /></p>
+        <p>I was having a hard time understanding the idea of investing, or using money to generate infinite wealth forever. I thought the S&amp;P 500 index generating an average return of 10% every year is too good to be true.</p>
+        <p><br /></p>
+        <p>Then it occurred to me that, the system run mostly based on exploitation, not innovation. Most of these companies grow because they pay their employees $1 for every $10 they make. The profit they made is reinvested to create innovation to help extract even more out of their workers.</p>
+        <p><br /></p>
+        <p>As result, the wealth of the controlling class will always rise exponentially faster than the poor and exploited.&nbsp;</p>
+        <p><br /></p>
+        <p>In Vancouver where I live, there are people who works full time while being homeless, and they are lucky to even have a job nowadays. This is modern day slavery made possible by the stock market. You have more money than you need, but instead of giving it to people who need them, you invest them to generate even more money.&nbsp;</p>
+        <p><br /></p>
+        <p>Just because everyone does it does not make it right. This is unsustainable and the growing wealth gap is tearing the societies apart.</p>
+        <p><br /></p>
+        <p>I pondered long and hard about human nature, and the point of life. We cannot simply convince people to be good nature, but we can create the environment conducive to it. That&apos;s when the idea of TownSquare emerged.&nbsp;</p>
+        <p><br /></p>
+        <p>I eventually quit my finance job and oriented towards IT to make it happen, learning how to code while holding menial jobs. The progress has been slow, but today after 10 years, I am finally able to launch the TownSquare movement.</p>
+        <p><br /></p>
+        <p>My dedication and confidence have never wavered in this journey, but I cannot accomplish it alone, and need good people to join the movement. If this sounds like you, please take part here.</p>
         <p><br /></p>
       </div>
     </>
